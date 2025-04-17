5364d91c85f66af5a45b494e3456fc7b2d29b3a2
Oktawiusz Doroszuk <doroszuk20@gmail.com>
2025-04-17 16:16:41 +0200

    [SCRUM-25] Search page

---
+++ KaraIOke/Controls/SearchSongView.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace KaraIOke.Controls
+{
+    public partial class SearchSongView : ContentView
+    {
+        public static readonly BindableProperty SongNameProperty = BindableProperty.Create(nameof(SongName), typeof(string), typeof(SearchSongView), string.Empty);
+
+        public string SongName
+        {
+            get => (string)GetValue(SearchSongView.SongNameProperty);
+            set => SetValue(SearchSongView.SongNameProperty, value);
+        }
+
+        public SearchSongView() {
+            InitializeComponent();
+        }
+    }
+}
