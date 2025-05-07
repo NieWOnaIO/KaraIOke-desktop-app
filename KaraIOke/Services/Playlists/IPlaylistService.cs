e3d7115d7a30b93831ae4d789cfbcd4b39dbd168
Zuzanna Stajniak <144373152+zuniak@users.noreply.github.com>
2025-05-08 01:10:09 +0200

    update service

--- KaraIOke/Services/Playlists/IPlaylistService.cs
+++ KaraIOke/Services/Playlists/IPlaylistService.cs
@@ -1,7 +1,9 @@
+using System.Collections.ObjectModel;
 using KaraIOke.Models;
 
 namespace KaraIOke.Services.Playlists;
 
 public interface IPlaylistService {
     Playlist GetPlaylist(string playlistName);
+    ObservableCollection<string> GetAllPlaylistsNames();
 }
