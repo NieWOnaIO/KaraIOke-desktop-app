bd7df015823d7b3a0038948e68343fa77e1a33ba
Oktawiusz Doroszuk <doroszuk20@gmail.com>
2025-05-22 13:32:26 +0200

    add mock

--- KaraIOke/Services/Download/DownloadMockService.cs
+++ KaraIOke/Services/Download/DownloadMockService.cs
@@ -4,7 +4,19 @@ namespace KaraIOke.Services.Download;
 
 public class DownloadMockService : IDownloadService
 {
-    public Task QueryDownload(Song song) { return Task.FromResult(0); }
-    public SongAudio GetSongAudio(Song song) { return new(); }
+
+    private SongAudio _songAudio = new();
+
+    public async Task QueryDownload(Song song)
+    {
+        var vocalsStream = FileSystem.OpenAppPackageFileAsync("vocals.mp3");
+        var noVocalsStream = FileSystem.OpenAppPackageFileAsync("no_vocals.mp3");
+
+        _songAudio = new SongAudio { NoVocals = await noVocalsStream, Vocals = await vocalsStream };
+    }
+    public SongAudio GetSongAudio(Song song)
+    {
+        return _songAudio;
+    }
 
 }
